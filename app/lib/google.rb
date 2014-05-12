42394d32fbeee88c09e8de388e5519ce0393a8df
Phil Callister <philcallister@gmail.com>
2014-05-12 08:55:47 -0500

    Formatting

--- app/lib/google.rb
+++ app/lib/google.rb
@@ -1,24 +1,24 @@
 module Google
 
-	class Direction
+  class Direction
 
-	  BASE_HTTP = "https://maps.googleapis.com/maps/api/directions/json"
+    BASE_HTTP = "https://maps.googleapis.com/maps/api/directions/json"
 
-	  def self.directions(origin, destination, waypoints=nil, &block)
-	    wpts = waypoints.inject('') { |r, e| r.empty? ? Geo::format(e) : "#{r}|#{Geo::format(e)}" } unless waypoints.nil?
-	    data = {origin: Geo::format(origin), destination: Geo::format(destination), waypoints: wpts, sensor: false}
-	    BW::HTTP.get(BASE_HTTP, {payload: data}) do |response|
-	      if response.ok?
-	        json = BW::JSON.parse(response.body.to_str)
+    def self.directions(origin, destination, waypoints=nil, &block)
+      wpts = waypoints.inject('') { |r, e| r.empty? ? Geo::format(e) : "#{r}|#{Geo::format(e)}" } unless waypoints.nil?
+      data = {origin: Geo::format(origin), destination: Geo::format(destination), waypoints: wpts, sensor: false}
+      BW::HTTP.get(BASE_HTTP, {payload: data}) do |response|
+        if response.ok?
+          json = BW::JSON.parse(response.body.to_str)
           route = json['routes'].first['overview_polyline']['points']
           block.call(route) if block
-	      else
-	        App.alert(response.error_message)
-	      end
-	    end
-	  end
+        else
+          App.alert(response.error_message)
+        end
+      end
+    end
 
-	end
+  end
 
   class Geo
 
